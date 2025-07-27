65c61b2069bb9a37bff6f0789eef45a6f0c756f7
¨dim50dim¨ <zheliezkov@gmail.com>
2025-07-27 20:20:02 +0200

    replace the array pets from Pet component to App component.

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,13 @@ import TimeArea from './components/TimeArea';
 import Footer from './components/Footer';
 import Pet from './components/Pet';
 
-
+const pets = [
+  { name: "Meowsalot", species: "cat", age: "5", id: 123456789 },
+  { name: "Barksalot", species: "dog", age: "3", id: 987654321 },
+  { name: "Fluffy", species: "rabbit", age: "2", id: 123123123 },
+  { name: "Purrsloud", species: "cat", age: "1", id: 456456456 },
+  { name: "Paws", species: "dog", age: "6", id: 789789789 }
+]
 function App() {
 
 
