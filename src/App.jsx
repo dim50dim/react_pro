1973ea65dea0d8ff7e47f9452764bc76c91bcdb8
¨dim50dim¨ <zheliezkov@gmail.com>
2025-07-28 13:50:26 +0200

    in App component change the place of array pets.

--- src/App.jsx
+++ src/App.jsx
@@ -9,15 +9,16 @@ import LikeArea from './components/LikeArea';
 import AddPetForm from './components/AddPetForm';
 
 
-const pets = [
+
+function App() {
+   const [pets,setPets] = useState(
+[
   { name: "Meowsalot", species: "cat", age: "5", id: 123456789 },
   { name: "Barksalot", species: "dog", age: "3", id: 987654321 },
   { name: "Fluffy", species: "rabbit", age: "2", id: 123123123 },
   { name: "Purrsloud", species: "cat", age: "1", id: 456456456 },
   { name: "Paws", species: "dog", age: "6", id: 789789789 }
-]
-function App() {
-
+])
 
   return (
     <>
