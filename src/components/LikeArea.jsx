570a48d0c7907efeef2b3c84200cffae002fd97d
¨dim50dim¨ <zheliezkov@gmail.com>
2025-07-28 11:16:37 +0200

    implemented two buttons increase and decrease statement.

--- src/components/LikeArea.jsx
+++ src/components/LikeArea.jsx
@@ -3,7 +3,9 @@ import React from 'react'
 const LikeArea = () => {
   return (
     <div>
-      
+        <button>Increase likes</button>
+        <button>Decrease likes</button>
+        <h2>This page has been liked 0 times</h2>
     </div>
   )
 }
