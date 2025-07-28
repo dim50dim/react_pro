34ce11538d85e73d2b0b90ab2f062069a8a91329
¨dim50dim¨ <zheliezkov@gmail.com>
2025-07-28 11:17:37 +0200

    included onClick ()

--- src/components/LikeArea.jsx
+++ src/components/LikeArea.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
 
 const LikeArea = () => {
+    
   return (
     <div>
-        <button>Increase likes</button>
-        <button>Decrease likes</button>
+        <button onClick={} >Increase likes</button>
+        <button onClick={}>Decrease likes</button>
         <h2>This page has been liked 0 times</h2>
     </div>
   )
