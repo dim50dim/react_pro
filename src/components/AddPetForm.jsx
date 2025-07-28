4ef1a44dd20f2f973cd42400cb0fa5d5153ceb98
¨dim50dim¨ <zheliezkov@gmail.com>
2025-07-28 14:00:26 +0200

    added useState to last created component.

--- src/components/AddPetForm.jsx
+++ src/components/AddPetForm.jsx
@@ -1,18 +1,21 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const AddPetForm = (props) => {
+    const [name,setName] = useState('');
+    const [species,setSpecies] = useState();
+    const [age,setAge] = useState()
 function handleSubmit(e) {
     e.preventDefault()
-      props.setPets();
+      props.setPets(prev => prev.concat() );
 }
   return (
     <div>
           <form onSubmit={handleSubmit}>
       <fieldset>
         <legend>Add New Pet</legend>
-        <input placeholder="Name" />
-        <input placeholder="species" />
-        <input placeholder="age in years" />
+        <input onChange={e=> setName(e.target.value)} placeholder="Name" />
+        <input onChange={e=> setSpecies(e.target.value)} placeholder="species" />
+        <input onChange={e=> setAge(e.target.value)} placeholder="age in years" />
         <button>Add Pet</button>
       </fieldset>
     </form>
