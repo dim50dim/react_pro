5c090587035d252c0db3ae222098c50a5b7016e4
¨dim50dim¨ <zheliezkov@gmail.com>
2025-07-28 10:15:25 +0200

    now, we are able to see seconds.

--- src/components/TimeArea.jsx
+++ src/components/TimeArea.jsx
@@ -1,7 +1,11 @@
 import React, { useState } from 'react'
 
 const TimeArea = () => {
-  const [time,setTime] = useState(new Date().toLocaleString())
+  const [time,setTime] = useState(new Date().toLocaleString());
+
+  setTimeout(() => {
+    setTime(new Date().toLocaleString())
+  }, 1000);
   return (
     <div>
        <p>The current time is {time}.</p>
