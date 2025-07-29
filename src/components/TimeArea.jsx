8662a0eb057939949bf04537f2ff445323e6f553
¨dim50dim¨ <zheliezkov@gmail.com>
2025-07-29 11:14:13 +0200

    used useEffect in TimeArea component.

--- src/components/TimeArea.jsx
+++ src/components/TimeArea.jsx
@@ -1,11 +1,10 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 
 const TimeArea = () => {
   const [time,setTime] = useState(new Date().toLocaleString());
-
-  setTimeout(() => {
-    setTime(new Date().toLocaleString())
-  }, 1000);
+    useEffect(() => {
+      setInterval(() => setTime(new Date().toLocaleString()),1000)
+    })
   return (
     <div>
        <p>The current time is {time}.</p>
