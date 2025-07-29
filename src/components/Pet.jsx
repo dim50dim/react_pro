8a4e02c4a417ac396914529e8c7d60b03a7bc8ad
¨dim50dim¨ <zheliezkov@gmail.com>
2025-07-29 10:43:35 +0200

    added button delete to this component.

--- src/components/Pet.jsx
+++ src/components/Pet.jsx
@@ -1,9 +1,13 @@
 
 
 const Pet = (props) => {
+  function handleDelete() {
+    
+  }
   return (
     <div>
            <li>{props.name} is a {props.species} and is {props.age} years old.</li>
+           <button onClick={handleDelete} >Delete</button>
     </div>
   )
 }
